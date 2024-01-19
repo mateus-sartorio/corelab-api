2eda079907822ac19c55605eb0a21b487f8d7ca1
MateusSartorio <mateussartorio39@gmail.com>
2024-01-18 22:21:40 -0300

    eslint configured

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,6 @@ import express from "express";
 const PORT = 3000;
 
 const app = express();
-app.listen(PORT, () => console.log(`Connected to db. Listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Connected to db. Listening on port ${PORT}`);
+});
