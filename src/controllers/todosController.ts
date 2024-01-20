8ba8528db3c6f3f0d9fb143ea440748b5e830b0a
MateusSartorio <mateussartorio39@gmail.com>
2024-01-19 22:05:35 -0300

    jest and supertest configured
    a mockup app was created to access a mockup database for testing, and some modifications had to be made to todosController

--- src/controllers/todosController.ts
+++ src/controllers/todosController.ts
@@ -1,35 +1,31 @@
 import { type Request, type Response } from "express";
 import { TodoModel } from "../models/todo";
 
-export async function getAllTodos(
-  request: Request,
-  response: Response,
-): Promise<void> {
+export async function getAllTodos(request: Request, response: Response): Promise<void> {
   try {
     const data = await TodoModel.find();
-    response.json(data);
-  } catch (e: any) {
-    response.status(400).json(e.message);
-  }
-}
-
-export async function getTodo(
-  request: Request,
-  response: Response,
-): Promise<void> {
-  try {
-    const id = request.params.id;
-    const data = await TodoModel.find({ _id: id });
     response.json(data);
   } catch (e: any) {
     response.status(404).json(e.message);
   }
 }
 
-export async function createTodo(
-  request: Request,
-  response: Response,
-): Promise<void> {
+export async function getTodo(request: Request, response: Response): Promise<void> {
+  try {
+    const id = request.params.id;
+    const data = await TodoModel.find({ _id: id });
+
+    if (data.length == 0) {
+      throw new Error("Todo not found");
+    }
+
+    response.json(data);
+  } catch (e: any) {
+    response.status(404).json(e.message);
+  }
+}
+
+export async function createTodo(request: Request, response: Response): Promise<void> {
   try {
     const { title, body, isFavorited } = request.body;
     const newTodo = new TodoModel({ title, body, isFavorited });
@@ -40,10 +36,7 @@ export async function createTodo(
   }
 }
 
-export async function updateTodo(
-  request: Request,
-  response: Response,
-): Promise<void> {
+export async function updateTodo(request: Request, response: Response): Promise<void> {
   try {
     const id = request.params.id;
     const data = await TodoModel.findOneAndUpdate({ _id: id }, request.body, {
@@ -55,13 +48,15 @@ export async function updateTodo(
   }
 }
 
-export async function deleteTodo(
-  request: Request,
-  response: Response,
-): Promise<void> {
+export async function deleteTodo(request: Request, response: Response): Promise<void> {
   try {
     const id = request.params.id;
     const data = await TodoModel.findOneAndDelete({ _id: id });
+
+    if (data == null) {
+      throw new Error("Todo not found");
+    }
+
     response.json(data);
   } catch (e: any) {
     response.status(404).json(e.message);
